ecf19737d786466e68ce17550c1c213553683075
Lukas Santiago <lukas.asant@gmail.com>
2020-11-05 00:32:13 -0300

    TryToTrackPointInPath

--- public/assets/classes/classes.js
+++ public/assets/classes/classes.js
@@ -12,13 +12,23 @@ class Point {
         this.column = column
         this.row = row
 
+        this.svgPoint = null
         this.svg = null
         this.used = false
+        this.disabled = false
+        this.letra = null
 
         this.drawSelf()
+        this.setSvgPoint()
 
         Point.all.push(this)
         return this
+    }
+
+    setSvgPoint() {
+        this.svgPoint = document.getElementsByTagName('svg')[0].createSVGPoint()
+        this.svgPoint.x = this.x
+        this.svgPoint.y = this.y
     }
 
     drawSelf() {
@@ -33,6 +43,15 @@ class Point {
             this.circle.fill = color
             text ? this.drawText() : false
             this.used = used
+            two.update()
+        }
+    }
+
+    disablePoint() {
+        if (this.used !== true) {
+            // this.circle.fill = color
+            this.circle.remove()
+            this.disabled = true
             two.update()
         }
     }
@@ -55,13 +74,63 @@ class Point {
         for (let row = 1; row <= Grid.rows + 1; row++) {
             let inThisRow = Point.all.filter(p => p.row == row)
             inThisRow.forEach(p =>
-                string += `(${p.used === true ? 'T' : 'F'}) `
+                string += `(${p.used === true ? p.letra : ' '}) `
             )
             string += '\n'
         }
         return string
 
     }
+
+    static possiblePointsInPath(from, to, returnArray = []) {
+        if (from.column != to.column || from.row != to.row) {
+            console.log(`(${from.row}, ${from.column}) (${to.row}, ${to.column})`);
+
+            let point = Point.get(from.row, from.column)
+
+            let svgPoint = document.getElementsByTagName('svg')[0].createSVGPoint();
+            svgPoint.x = point.x;
+            svgPoint.y = point.y;
+            returnArray.push(svgPoint)
+
+            // Bar.all[0].line._renderer.elem.isPointInStroke(svgPoint);
+
+            // //TODO verificar se a barra está no real lugar
+            // point.disablePoint()
+
+            Point.possiblePointsInPath(Point.pickOne(from, to), to, returnArray)
+        }
+        // returnArray.shift()
+        return returnArray
+    }
+    static pickOne(from, to) {
+        let p = {}
+        if (from.column != to.column) {
+            if (from.column < to.column) {
+                p.column = from.column + 1
+            }
+            else {
+                p.column = from.column - 1
+            }
+        }
+        else {
+            p.column = to.column
+        }
+        if (from.row != to.row) {
+            if (from.row < to.row) {
+                p.row = from.row + 1
+            }
+            else {
+                p.row = from.row - 1
+            }
+        }
+        else {
+            p.row = to.row
+        }
+        return p
+    }
+
+    static get(row, column) { return Point.all.find(p => p.row == row && p.column == column) }
 }
 
 class Grid {
@@ -154,6 +223,26 @@ class Bar {
     get from() { return this._from }
     get to() { return this._to }
 
+    get angle() {
+        let diffRow = (this._to.row - this._from.row) * -1
+        let diffCol = (this._to.column - this._from.column)
+
+        let main = ((Math.asin(diffRow / Math.hypot(diffRow, diffCol)) * 180 / Math.PI)).toFixed(2)
+        if (diffRow >= 0 && diffCol >= 0) {
+            //console.log('primeiro quadrante');
+            return main
+        } else if (diffRow >= 0 && diffCol < 0) {
+            //console.log('segundo quadrante');
+            return (90 - main) + 90
+        } else if (diffRow < 0 && diffCol <= 0) {
+            //console.log('terceiro quadrante');
+            return (90 - main) + 90
+        } else {
+            //console.log('quarto quadrante');
+            return (90 - main) + 180
+        }
+    }
+
     constructor(from, to) {
         if (!from instanceof Point && !to instanceof Point)
             throw new error('Cannot Create Bar')
@@ -161,16 +250,49 @@ class Bar {
         this._from = from
         this._to = to
 
-        //TODO UsePoints
-        from.usePoint('red', true, true)
-        to.usePoint('red', true, true)
-        //TODO trackPointsInPath
-        this.trackPointsInPath()
+        this.usePoints(from, to)
+
+        //TODO possiblePointsInPath
+        // let pointsThatCanBeInPath = Point.possiblePointsInPath(this._from, this._to)
+
         this.drawSelf()
-        two.update()
+        this.setBarSVG()
+
+
+        // console.log(pointsThatCanBeInPath);
+        // pointsThatCanBeInPath.shift()
+        let pointsInPath = Point.all.filter(p => this.svg.isPointInStroke(p.svgPoint))
+        pointsInPath.pop()
+        pointsInPath.shift()
+
+        console.log(pointsInPath);
+        pointsInPath.forEach(p => p.disablePoint())
+
+
+
 
         Bar.all.push(this)
+
+
+
         return this
+    }
+
+    setBarSVG() {
+        two.update()
+        let bar = this
+        let barRenderEvent = function () {
+            bar.svg = bar.line._renderer.elem
+        }
+
+        two.bind('render', barRenderEvent)
+        two.render()
+        two.unbind('render', barRenderEvent)
+    }
+
+    usePoints(from, to) {
+        from.usePoint('red', true, true)
+        to.usePoint('red', true, true)
     }
 
     drawSelf() {
@@ -184,13 +306,6 @@ class Bar {
 
         barsGroup.add(this.line)
     }
-
-    trackPointsInPath() {
-        let from = this.from.circle.position
-        let to = this.to.circle.position
-
-
-    }
 }
 
-export { Point, Grid, Ruler, Bar }
+export { Point, Grid, Ruler, Bar }  
